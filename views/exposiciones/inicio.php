6ffbf3d5202e3c82997038280f494cab1cb4c670
mercedeslobos <lobos.mercedes@gmail.com>
2019-10-19 21:56:59 -0300

    Modificaciones de inicio incluyo pdf manual de usuario

--- views/exposiciones/inicio.php
+++ views/exposiciones/inicio.php
@@ -7,11 +7,20 @@ $layout = 'internal.php';
 $this->title = 'Inicio';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-inicio">
-    <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
-        Esta página debería contener alguna explicación sobre el uso del sistema
+<div class="site-inicio">
+    <div class="container">
+    <p style = "font-size:20px; color: white; text-align: justify; margin: 10px 250px; padding: 10px;">
+    El desarrollo de “ReDAV” tiene como propósito brindar una herramienta a la División de accidentes viales que agiliza el proceso de registración de siniestros viales. 
     </p>
+    <p style = "font-size:20px; color: white; text-align: justify; margin: 10px 250px; padding: 10px;">
+    Esta Pagina Web, está construida para Generar un registro de Exposiciones por colisión en formato digital, incluyendo los registros antiguos y Gestionara datos estadísticos los cuales serán publicados y comunicados a los ámbitos solicitados, estas operaciones son realizadas en las oficinas de la División de Accidentes Viales evitando tener un archivo físico dentro de la institución.
+    </p>
+
+    <p style = "text-align: center;">
+    <a href="documentos/Manual de usuario - ReDAV Web.pdf" style = "color: white;" >Ver el Manual de Usuario del sitio Web ReDAV</a>
+    </p>
+    </div>
+
 
 </div>
